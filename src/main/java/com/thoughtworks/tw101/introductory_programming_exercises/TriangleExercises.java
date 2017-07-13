32fb341abdfee0030325df738390fd9b86e70fab
ledingto <edington.laura@gmail.com>
2017-07-13 12:47:00 -0700

    basic completion with loops

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
@@ -3,8 +3,13 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 public class TriangleExercises {
     public static void main(String[] args) {
         drawAnAsterisk();
+        System.out.println();
+        System.out.println();
         drawAHorizontalLine(8);
+        System.out.println();
+        System.out.println();
         drawAVerticalLine(3);
+        System.out.println();
         drawARightTriangle(3);
     }
 
@@ -12,14 +17,16 @@ public class TriangleExercises {
 //    Print one asterisk to the console.
 //    Example: *
     private static void drawAnAsterisk() {
-
+        System.out.print("*");
     }
 
 //    Draw a horizontal line
 //    Given a number n, print n asterisks on one line.
 //    Example when n=8:  ********
     private static void drawAHorizontalLine(int n) {
-
+        for (int i=0; i<n; i++) {
+            System.out.print("*");
+        }
     }
 
 //    Draw a vertical line
@@ -29,7 +36,9 @@ public class TriangleExercises {
 //            *
 //            *
     private static void drawAVerticalLine(int n) {
-
+        for (int j=0; j<n; j++) {
+            System.out.println("*");
+        }
     }
 
 //    Draw a right triangle
@@ -39,6 +48,13 @@ public class TriangleExercises {
 //            **
 //            ***
     private static void drawARightTriangle(int n) {
-
+        // number of rows
+        for (int i=1; i<n+1; i++) {
+            // number of columns (stars)
+            for (int j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
