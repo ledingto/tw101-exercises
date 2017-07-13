d977b2c80606f0007d78c48b4da6717ffeb69f54
ledingto <edington.laura@gmail.com>
2017-07-13 14:13:34 -0700

    Prime factors found; loop through factors then check if prime

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -1,19 +1,60 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
 //  Prime Factors Exercise
-//  Write a method generate(int n) that given an integer N will return a list of integers such that the numbers
-//  are the factors of N and are arranged in increasing numerical order.
+//  Write a method generate(int n) that given an integer n will return a list of integers such that the numbers
+//  are the prime factors of N and are arranged in increasing numerical order.
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+
+        List<Integer> primeFactors1 = generate(30);
+        List<Integer> primeFactors2 = generate(1);
+        List<Integer> primeFactors3 = generate(79);
+        List<Integer> primeFactors4 = generate(12);
+        List<Integer> primeFactors5 = generate(1000);
+
+        System.out.print(primeFactors1);
     }
 
+    // factor of n: n%i==0
+    // prime:
     private static List<Integer> generate(int n) {
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=1; i<=n; i++) {
+            if (n%i==0) {
+                if (isPrime(i)) {
+                    list.add(i);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        else if (n==2) {
+            return true;
+        }
+        else if (n%2==0) {
+            return false;
+        }
+        // check if divisible by odd numbers (because if divisible by even number, already caught by n%2==0 )
+        // prime definition: only divisible by 1 and itself
+        else {
+            for (int i=3; i<n; i+=2) {
+                if (n%i==0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
