48e352280030bb7c4ede43474730695809e95145
ledingto <edington.laura@gmail.com>
2017-07-12 22:43:29 -0700

    Guessing game with guess history

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/GuessingGame.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/GuessingGame.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class GuessingGame {
 
     private int randNum;
+    private int guessCount = 0;
 
     Random generator = new Random();
 
@@ -18,7 +19,12 @@ public class GuessingGame {
         randNum = generator.nextInt(upperBound);
     }
 
+    public int getGuessCount() {
+        return guessCount;
+    }
+
     public String guess(int guess) {
+        guessCount++;
         if (guess>randNum) {
             return "You guessed too high. Please guess again: ";
         }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Main.java
@@ -5,12 +5,13 @@ package com.thoughtworks.tw101.exercises.exercise8;
 // where the user entered a String that is not parseable.
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String msg;
+        ArrayList<Integer> guessHistory = new ArrayList<Integer>();
 
         // create game, which generates the random number to guess between 0 and 100
         GuessingGame game = new GuessingGame(101);
@@ -18,11 +19,25 @@ public class Main {
         // prompt user
         System.out.print("Please guess an integer between 0 and 100: ");
 
+        String msg;
         do {
-            msg = game.guess(scanner.nextInt());
+            int userGuess = scanner.nextInt();
+            guessHistory.add(userGuess);
+            msg = game.guess(userGuess);
             System.out.print(msg);
         }
         while (!(msg.equals("You win!")));
+
+        System.out.println("\nYou guessed " + game.getGuessCount() + " times.");
+        System.out.print("Your guesses were: ");
+        for (int i=0; i<guessHistory.size(); i++) {
+            if (i==guessHistory.size()-1) {
+                System.out.println(guessHistory.get(i) + ".");
+            }
+            else {
+                System.out.print(guessHistory.get(i) + ", ");
+            }
+        }
 
     }
 }
