5d5bae7947927caaaeea17babe6b1d3f185bf41b
ledingto <edington.laura@gmail.com>
2017-07-12 22:25:54 -0700

    Exercise8 complete with encapsulation

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Main.java
@@ -4,8 +4,25 @@ package com.thoughtworks.tw101.exercises.exercise8;
 // once they guess correctly. Use an ArrayList to store the guesses. Catch the NumberFormatException and handle the case
 // where the user entered a String that is not parseable.
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String msg;
+
+        // create game, which generates the random number to guess between 0 and 100
+        GuessingGame game = new GuessingGame(101);
+
+        // prompt user
+        System.out.print("Please guess an integer between 0 and 100: ");
+
+        do {
+            msg = game.guess(scanner.nextInt());
+            System.out.print(msg);
+        }
+        while (!(msg.equals("You win!")));
 
     }
 }
